49a0f0dc893325a6c75d9a377b8dd7af426b5a64
Tri Tac Le <tritac.le@gmail.com>
2025-05-30 21:50:22 +0200

    [feat]: fetching data frmo kanye and motivational quote API

--- src/api_clients/bible_client.py
+++ src/api_clients/bible_client.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_URL_BIBLE = os.getenv("BASE_URL_BIBLE")
 
-def get_verse_data(testament: str = ""):
+def fetch_bible_verse_data(testament: str = "") -> dict | None:
   """
   testament is OT or NT
   """
@@ -20,16 +20,15 @@ def get_verse_data(testament: str = ""):
     print(f"Failed to retrieve data. Status: {response.status_code}, error: {exception}")
     return None
 
-def get_random_verse(total_verse: int = 1, testament: str = ""):
+def fetch_bible_verse(total_verse: int = 1, testament: str = ""):
   all_verses_lst = []
   for _ in range(total_verse):
-    data = get_verse_data(testament)
+    data = fetch_bible_verse_data(testament)
     if not data or "random_verse" not in data:
       print("No verse returned from get_verse_data()")
       break
-
     random_verse = data["random_verse"]
-    
+  
     verse_entry = [
       random_verse,  
       random_verse["book"],
@@ -37,15 +36,15 @@ def get_random_verse(total_verse: int = 1, testament: str = ""):
       random_verse["verse"],
       random_verse["text"]
     ]
-
     all_verses_lst.append(verse_entry)
+    
   return all_verses_lst
 
 if __name__ == "__main__":
-  verses = get_random_verse(1, "")
+  verses = fetch_bible_verse(1, "")
   
   if verses:
-    for i in range(len(verses)):
-      random_verse_dict, book, chapter, verse, text = verses[i]
+    for verse in range(len(verses)):
+      random_verse_dict, book, chapter, verse, text = verses[verse]
       print(f"{book} {chapter}:{verse} - {text}")
 
